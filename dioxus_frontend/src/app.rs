aa70ed3c1b25ea5a935855a15d822a6039f4f5e0
killian <oss@xtrm.me>
2024-11-11 18:07:25 +0100

    fix(aftertime): remove the debug line that prevents the site from rendering... ha.
    Signed-off-by: killian <oss@xtrm.me>

--- dioxus_frontend/src/app.rs
+++ dioxus_frontend/src/app.rs
@@ -1,52 +1,41 @@
 use dioxus::prelude::*;
 use crate::assets::*;
 use crate::components::navbar::Navbar;
-use crate::pages::blog::Blog;
 use crate::routes::Route;
 
 #[component]
 pub(crate) fn AppLayout() -> Element {
     let route: Route = use_route();
     let is_home = route == Route::Home {};
-    if is_home {
-        return rsx! {};
-    }
     rsx! {
         div {
             class: "overflow-x-hidden bg-auto bg-center bg-fixed w-full h-full text-white",
             background_image: "url({BACKGROUND_IMAGE})",
 
-            Navbar {
-                name: "rsty".to_string(),
-                logo_uri: APP_LOGO,
+            if !is_home {
+                Navbar {
+                    name: "rsty".to_string(),
+                    logo_uri: APP_LOGO,
 
-                div {
-                    class: "flex flex-row gap-4 items-center",
-                    Link {
-                        to: Route::Home {},
-                        button {   
-                            "Home"
-                        }
-                    }
-                    if is_home {
-                        a {
-                            href: "#contact",
-                            button {
-                                "Contact"
+                    div {
+                        class: "flex flex-row gap-4 items-center",
+                        Link {
+                            to: Route::Home {},
+                            button {   
+                                "Home"
                             }
                         }
-                    } else {
                         Link {
                             to: Route::Home { }, //TODO: figure out how to apply a fragment, dioxus seems to strip it out?
                             button {
                                 "Contact"
                             }
                         }
-                    }
-                    Link {
-                        to: Route::Blog {},
-                        button {
-                            "News"
+                        Link {
+                            to: Route::Blog {},
+                            button {
+                                "News"
+                            }
                         }
                     }
                 }
